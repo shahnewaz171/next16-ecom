5c56df2df54a9ba7d83fc4ef023b4979acef2164
shahnewaz171 <shahnewaz601@gmail.com>
2025-12-27 13:49:47 +0600

    chore(hooks): add Husky pre-commit hooks and lint-staged config

---
+++ lint-staged.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+    'biome check --write --unsafe --no-errors-on-unmatched --files-ignore-unknown=true'
+  ],
+  '*.{css,html,yml,yaml}': ['biome check --write --no-errors-on-unmatched --files-ignore-unknown=true']
+};
