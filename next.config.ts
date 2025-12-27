583e939a55bf8ed9886fd9aec48ef2216c2af2d6
shahnewaz171 <shahnewaz601@gmail.com>
2025-12-27 12:42:11 +0600

    chore: update next config and add husky

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true
-  // cacheComponents: true
+  reactCompiler: true,
+  cacheComponents: true,
+  transpilePackages: [],
+  experimental: {
+    optimizePackageImports: []
+  },
+  compiler: {
+    removeConsole: {
+      exclude: ['error']
+    }
+
+    // executes after production build compilation completes.
+    // runAfterProductionCompile: async ({ projectDir, distDir }) => {
+    // }
+  }
 };
 
 export default nextConfig;
