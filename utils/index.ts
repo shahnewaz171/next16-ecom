d65a9abc5799f46cc474aa06d11d8da5b87b98bd
shahnewaz171 <shahnewaz601@gmail.com>
2026-04-03 09:52:52 +0600

    fix: type issue of util function

--- utils/index.ts
+++ utils/index.ts
@@ -3,10 +3,10 @@ export const simulateDelay = (delay: number = 1000): Promise<void> =>
 
 export const getUidWithPrefix = (prefix: string, index: number | string) => `${prefix}-${index}`;
 
-export const debounce = <F extends (...args: any[]) => void>(func: F, waitFor: number) => {
+export const debounce = <F extends (...args: Parameters<F>) => void>(func: F, waitFor: number) => {
   let timeout: ReturnType<typeof setTimeout>;
 
-  return function (this: any, ...args: Parameters<F>) {
+  return function (this: ThisParameterType<F>, ...args: Parameters<F>) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(this, args), waitFor);
   };
